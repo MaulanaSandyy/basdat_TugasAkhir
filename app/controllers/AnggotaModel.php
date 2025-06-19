055ccccf32bd77e380fd7265582e834f2accd053
MaulanaSandyy <maulanasandy.official@gmail.com>
2025-06-19 19:40:16 +0700

    perbaiki code update anggota model yang fix

--- app/controllers/AnggotaModel.php
+++ app/controllers/AnggotaModel.php
@@ -1,4 +1,35 @@
-public function getRiwayatPinjaman($idAnggota) {
+<?php
+require_once __DIR__ . '/../models/AnggotaModel.php';
+
+class AnggotaController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new AnggotaModel();
+    }
+
+    public function index() {
+        $anggota = $this->model->getAllAnggota();
+        require_once __DIR__ . '/../views/anggota/index.php';
+    }
+
+    public function create() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nama = $_POST['nama'];
+            $alamat = $_POST['alamat'];
+            $noTelp = $_POST['no_telp'];
+            
+            if ($this->model->createAnggota($nama, $alamat, $noTelp)) {
+                header('Location: /anggota');
+                exit;
+            } else {
+                $error = "Gagal menambahkan anggota";
+            }
+        }
+        require_once __DIR__ . '/../views/anggota/create.php';
+    }
+
+    public function getRiwayatPinjaman($idAnggota) {
     $sql = "SELECT dp.IdPinjam, db.Judul AS JudulBuku, dp.TanggalPinjam, dp.Jumlah, 
                    CASE WHEN dk.IdKembali IS NULL THEN 'Dipinjam' ELSE 'Dikembalikan' END AS Status
             FROM DaftarPinjaman dp
@@ -15,3 +46,7 @@ public function getRiwayatPinjaman($idAnggota) {
     }
     return $result;
 }
+
+    // Metode lainnya (edit, delete, view)...
+}
+?>
