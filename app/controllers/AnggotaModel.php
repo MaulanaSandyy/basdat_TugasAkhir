e50ff8b1bf098b34142e91fda98262ac979a341b
MaulanaSandyy <maulanasandy.official@gmail.com>
2025-06-19 19:38:51 +0700

    update getRiwayatPinjaman untuk menampilkan status pinjaman
    - Tambah kolom status 'Dipinjam' atau 'Dikembalikan' menggunakan LEFT JOIN dengan tabel pengembalian
    - Urutkan berdasarkan TanggalPinjam secara descending
    - Berguna untuk menampilkan riwayat pinjaman lebih informatif

--- app/controllers/AnggotaModel.php
+++ app/controllers/AnggotaModel.php
@@ -1,34 +1,17 @@
-<?php
-require_once __DIR__ . '/../models/AnggotaModel.php';
-
-class AnggotaController {
-    private $model;
-
-    public function __construct() {
-        $this->model = new AnggotaModel();
+public function getRiwayatPinjaman($idAnggota) {
+    $sql = "SELECT dp.IdPinjam, db.Judul AS JudulBuku, dp.TanggalPinjam, dp.Jumlah, 
+                   CASE WHEN dk.IdKembali IS NULL THEN 'Dipinjam' ELSE 'Dikembalikan' END AS Status
+            FROM DaftarPinjaman dp
+            JOIN DaftarBuku db ON dp.IdBuku = db.IdBuku
+            LEFT JOIN DaftarPengembalian dk ON dp.IdPinjam = dk.IdPinjam
+            WHERE dp.IdAnggota = ?
+            ORDER BY dp.TanggalPinjam DESC";
+    
+    $params = array($idAnggota);
+    $stmt = sqlsrv_query($this->db, $sql, $params);
+    $result = array();
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $result[] = $row;
     }
-
-    public function index() {
-        $anggota = $this->model->getAllAnggota();
-        require_once __DIR__ . '/../views/anggota/index.php';
-    }
-
-    public function create() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $nama = $_POST['nama'];
-            $alamat = $_POST['alamat'];
-            $noTelp = $_POST['no_telp'];
-            
-            if ($this->model->createAnggota($nama, $alamat, $noTelp)) {
-                header('Location: /anggota');
-                exit;
-            } else {
-                $error = "Gagal menambahkan anggota";
-            }
-        }
-        require_once __DIR__ . '/../views/anggota/create.php';
-    }
-
-    // Metode lainnya (edit, delete, view)...
+    return $result;
 }
-?>
